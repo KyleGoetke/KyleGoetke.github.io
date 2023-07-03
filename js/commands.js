3f562162fba7920a1dffeb9cbcd84483e7b84e68
Kyle Goetke <goetkek@protonmail.com>
2023-07-03 11:42:02 -0400

    Testing new terminal site design

---
+++ js/commands.js
@@ -0,0 +1,117 @@
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/kyle-goetke/";
+var github = "https://github.com/kylegoetke/";
+var email = "mailto:kyle.goetke@gmail.com";
+
+whois = [
+    "<br>",
+    "I'm a student, a musician, a computer hardware enthusiast, and a heavy sleeper.",
+    "I got my first computer when I was twelve years old and fell in love with technology.",
+    "Since then I've accumulated knowledge and hands-on experience in a range of technical roles.",
+    "<br>",
+    "I'm attending Drexel University, where I'm studying Computing and Security Technology.",
+    "I enjoy playing the drums, assembling computers, exploring Philadelphia, and rolling d20s.",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The answer to this question cannot be given, it must be sought out.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'linkedin     <a href="' + linkedin + '" target="_blank"><span class="command">linkedin/kyle-goetke</span>' + "</a>",
+    'github       <a href="' + github + '" target="_blank"><span class="command">github/kylegoetke</span>' + "</a>",
+    "<br>"
+];
+
+education = [
+    "<br>",
+    '<span class="command">B.S. Computing and Security Technology</span>             Expected Graduation: 2025',
+    "Drexel University                                  Cumulative GPA: 3.92",
+    "<br>",
+    '<span class="command">High School Diploma</span>                                Graduated 2020',
+    "Union County Academy for Information Technology    Cumulative GPA: 3.89",
+    "<br>",
+];
+
+experience = [
+    "<br>",
+    '<span class="command">Security Analyst</span>                             September 2022 &ndash; March 2023',
+    "Susquehanna International Group              Bala Cynwyd, PA",
+    "- Automated phishing email detection, preventing dozens of emails from reaching end users weekly",
+    "- Investigated and responded to staged incidents, active threats, and policy violations",
+    "- Designed and implemented a PowerShell solution to automate adjustments to SIEM rule attributes, removing this task from analysts' weekly responsibilities",
+    "- Carried out regular reviews of potentially malicious emails and analyzed IOCs to prevent attacks",
+    "- Onboarded and trained Drexel co-ops with a variety of technical backgrounds",
+    "<br>",
+    '<span class="command">Associate DevOps Engineer</span>                    September 2021 &ndash; March 2022',
+    "Health Union                                 Philadelphia, PA",
+    "- Utilized Amazon Web Services and Docker to construct a CI/CD service and automate IaC deployment",
+    "- Led the development of a comprehensive guide for company-wide use, tailored to non-technical users",
+    "- Adhered to Agile Scrum development practices to guide development and task completion",
+    "- Managed infrastructure for over forty websites using Terraform, resulting in increased reliability and consistency in deployment",
+    "<br>",
+    '<span class="command">IT Intern</span>                                    June 2019 &ndash; June 2021',
+    "Union County Vocational-Technical Schools    Scotch Plains, NJ",
+    "- Resolved issues across a campus-wide network including hardware repair and software configuration",
+    "- Trained eight new interns to prepare them for success in the role throughout the following year",
+    "- Collaborated with faculty to find solutions for issues with productivity and connectivity",
+    "<br>",
+    '<span class="command">Volunteer</span>                                    July 2017 & July 2019',
+    "Appalachia Service Project                   Virginia & West Virginia",
+    "- Helped repair homes and improve living conditions for low-income families",
+    "- Cooperated with group members to achieve our goal in a limited time frame",
+    "<br>",
+    '<span class="command">Technology Assistant</span>                         June 2016 &ndash; September 2018',
+    "New Providence School District               New Providence, NJ",
+    "- Responsible for organizing and installing new hardware and software",
+    "- Cooperated with team members to integrate new and existing technology systems",
+    "- Assisted faculty and staff with resolving technology issues",
+    "<br>",
+];
+
+projects = [
+    "<br>",
+    '<span class="command">Platform Panic (term project) | Developer</span>    January 2021 &ndash; June 2021',
+    "- Developed a game inspired by classic platformers using HTML, CSS, and JavaScript",
+    "- Utilized Agile development practices to manage group workflow",
+    "- Collaborated, committed changes, and coordinated work using Git",
+    "<br>",
+    '<span class="command">Custom-Built Desktop Computers</span>               June 2015 &ndash; Present',
+    "- Researched and identified compatible hardware components",
+    "- Assembled and actively maintain computer hardware",
+    "<br>",
+];
+
+honors = [
+    "<br>",
+    '<span class="command">Dean\'s List</span> Drexel University                Fall 2020 &ndash; Present',
+    '<span class="command">Eagle Scout</span> BSA Troop 63 &ndash; New Providence    November 2019',
+    "<br>",
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>        Who is Kyle?',
+    '<span class="command">whoami</span>       Who are you?',
+    '<span class="command">education</span>    View education',
+    '<span class="command">experience</span>   View experience',
+    '<span class="command">projects</span>     View technical projects',
+    '<span class="command">honors</span>       View honors and awards',
+    '<span class="command">social</span>       Display social networks',
+    '<span class="command">history</span>      Display command history',
+    '<span class="command">help</span>         You\'re looking at it',
+    '<span class="command">email</span>        Email me',
+    '<span class="command">clear</span>        Clear the terminal',
+    '<span class="command">banner</span>       Display the welcome message',
+    "<br>",
+];
+
+banner = [
+    "<br>Hello! I'm Kyle. Welcome to my interactive web terminal!",
+    "For a list of available commands, type <span class=\"command\">'help'</span>.",
+    "<br>"
+];
